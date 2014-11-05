59643d83ef230ae77b699c4a3279fda0df950f1c
awailly <aurelien.wailly@gmail.com>
2014-11-05 13:19:25 +0100

    Automatic PYTHONPATH

--- vespa/starter.py
+++ vespa/starter.py
@@ -24,6 +24,13 @@
 """
 Starter
 """
+import sys
+import os
+ 
+DOSSIER_COURRANT = os.path.dirname(os.path.abspath(__file__))
+DOSSIER_PARENT = os.path.dirname(DOSSIER_COURRANT)
+sys.path.append(DOSSIER_PARENT)
+
 from model import Model
 from view import View
 from controller import Controller
