5e55c7c588360267a10701b82efecedccfee9e7d
awailly <aurelien.wailly@gmail.com>
2014-11-05 13:47:53 +0100

    First tests
    - Reset log_pipe.py and config.ini
    - First tests for node.py

--- vespa/starter.py
+++ vespa/starter.py
@@ -26,7 +26,7 @@ Starter
 """
 import sys
 import os
- 
+
 DOSSIER_COURRANT = os.path.dirname(os.path.abspath(__file__))
 DOSSIER_PARENT = os.path.dirname(DOSSIER_COURRANT)
 sys.path.append(DOSSIER_PARENT)
--- vespa/tests/test_node.py
+++ vespa/tests/test_node.py
@@ -1,7 +1,7 @@
 import unittest 
 import sys
 import os
- 
+
 DOSSIER_COURRANT = os.path.dirname(os.path.abspath(__file__))
 DOSSIER_PARENT = os.path.dirname(DOSSIER_COURRANT)
 sys.path.append(DOSSIER_PARENT)
@@ -12,17 +12,31 @@ from node import Node
 class TestCrypto(unittest.TestCase):
     def test_encrypt(self):
         n = Node('testnode', "127.0.0.1", 1337, None, run=False)
-        iv = 0
-        msg = "hello"
-        c = n.__encrypt(m, iv)
-        self.assertEqual(c, 'pomme')
+        iv = "%16i" % 0
+        m = "hello"
+        c = n._encrypt(m, iv)
+        self.assertEqual(c, ('Y\xc8\x83V\xe4', '2dc4d9ee2b2f519dae0d9e55e1a3e512f30f2738'))
 
     def test_decrypt(self):
         n = Node('testnode', "127.0.0.1", 1337, None, run=False)
-        iv = 0
-        msg = "hello"
-        c = n.__decrypt(m, iv, auth)
-        self.assertEqual(c, 'pomme')
+        iv = "%16i" % 0
+        m = "hello"
+        c = n._decrypt(m, iv, n._checksum(m))
+        self.assertEqual(c, 'Y\xf4\x14>\x1d')
+
+    def test_checksum(self):
+        n = Node('testnode', "127.0.0.1", 1337, None, run=False)
+        c = n._checksum("hello")
+        self.assertEqual(c, 'aaf4c61ddcc5e8a2dabede0f3b482cd9aea9434d')
+
+    def test_encrypt_decrypt(self):
+        n = Node('testnode', "127.0.0.1", 1337, None, run=False)
+        iv = "%16i" % 0
+        m = "hello"
+        c = n._encrypt(m, iv)
+        m_d = c[0]
+        d = n._decrypt(m_d, iv, n._checksum(m_d))
+        self.assertEqual(d, m)
  
 if __name__ == '__main__':
     unittest.main()
