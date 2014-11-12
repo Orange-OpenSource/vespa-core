63a27c4ca01c49f2357e2bb85cbb58ba68aae242
Aurélien Wailly <aurelien.wailly@gmail.com>
2014-11-12 09:42:48 +0100

    Documentation for agent_controller_floodlight

--- vespa/agent_controller_floodlight.py
+++ vespa/agent_controller_floodlight.py
@@ -21,7 +21,8 @@
 # along with VESPA.  If not, see <http://www.gnu.org/licenses/>.
 
 """
-Agent to wrap Gandalf's controller
+Agent to wrap Gandalf's controller. Based on floodlight, it can be a nice
+start for a full API against floodlight.
 """
 
 from logging import *
@@ -33,6 +34,12 @@ import random
 
 
 class Agent_Controller_Floodlight(Agent_Controller):
+    """Flag a mac address as suspicious and gather statistics for local
+    links
+
+    :return: The wrapper to the OMN controller
+    :rtype: Node
+    """
 
     def __init__(self, name, host, port, master, run=False):
         self.controller_ip = "12.0.0.20:9999"
@@ -62,18 +69,47 @@ class Agent_Controller_Floodlight(Agent_Controller):
         return the_page
 
     def alert_ip(self, ip, mac):
+        """Block a tuple (ip,mac) with SDN
+
+        :param str IP: The IP to block (for future)
+        :param str mac: The associated MAC address (needed)
+        :return: The controller response
+        :rtype: str
+        """
         return self.block_hackers(mac)
 
     def status_hackers(self):
+        """Get the status of a tuple (ip,mac) with SDN
+
+        :return: The controller response
+        :rtype: str
+        """
         return self._send_controller('status_hackers')
 
     def release_hackers(self):
+        """Release all tuples (ip,mac) with SDN
+
+        :return: The controller response
+        :rtype: str
+        """
         return self._send_controller('release_hackers')
 
     def block_hackers(self, mac):
+        """Block a MAC address with SDN
+
+        :param str mac: The associated MAC address
+        :return: The controller response
+        :rtype: str
+        """
         return self._send_controller('?mac=%s' % mac)
 
     def get_topology(self, cmd='wm/topology/switchclusters/json'):
+        """Get the current topology of the SDN network
+
+        :param str cmd: The floodlight URL to grab the topology
+        :return: The list of nodes and links detected
+        :rtype: dict
+        """
         topo = self._send_controller(cmd)
         jt = json.loads(topo)
 
@@ -98,5 +134,11 @@ class Agent_Controller_Floodlight(Agent_Controller):
         return {'links': edges, 'nodes': nodes}
 
     def get_link_stats(self, cmd='wm/topology/links/json'):
+        """Get links statistics over the floodlight controller
+
+        :param str cmd: The floodlight URL to grab the links statistics
+        :return: The controller response
+        :rtype: str
+        """
         links = self._send_controller(cmd)
         return links
