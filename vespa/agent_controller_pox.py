e91c61281df0183c09f1cd32d4e6da5695f315d6
Aurélien Wailly <aurelien.wailly@gmail.com>
2014-11-13 12:54:16 +0100

    Documentation for agent_controller_pox

--- vespa/agent_controller_pox.py
+++ vespa/agent_controller_pox.py
@@ -21,7 +21,9 @@
 # along with VESPA.  If not, see <http://www.gnu.org/licenses/>.
 
 """
-Agent to wrap Gandalf's controller
+Agent to wrap the POX python SDN controller. It require some modification on
+the other side too. You can follow the mac address blocking tutorial on the
+POX website.
 """
 
 from logging import *
@@ -40,6 +42,12 @@ mychannel = 'mac_redir'
 
 
 class Agent_Controller_Pox(Agent_Controller):
+    """Flag a mac address as suspicious and gather statistics for local
+    links
+
+    :return: The wrapper to the OMN controller
+    :rtype: Node
+    """
 
     def __init__(self, name, host, port, master, run=False):
         self.controller_ip = '10.193.163.27'
@@ -119,19 +127,54 @@ class Agent_Controller_Pox(Agent_Controller):
         return json.loads(d)
 
     def alert_ip(self, ip, mac):
+        """Block a tuple (ip,mac) with SDN
+
+        :param str IP: The IP to block (for future)
+        :param str mac: The associated MAC address (needed)
+        :return: The controller response
+        :rtype: str
+        """
         return self.block_hackers()
 
     def status_hackers(self):
+        """Get the status of a tuple (ip,mac) with SDN
+
+        :return: The controller response
+        :rtype: str
+        """
         return self._send_controller('status_hackers')
 
     def release_hackers(self):
+        """Release all tuples (ip,mac) with SDN
+
+        :return: The controller response
+        :rtype: str
+        """
         return self._send_controller('release_hackers')
 
     def block_hackers(self):
+        """Block a MAC address with SDN
+
+        :param str mac: The associated MAC address
+        :return: The controller response
+        :rtype: str
+        """
         return self._send_controller('block_hackers')
 
     def get_topology(self, cmd='get_topology'):
+        """Get the current topology of the SDN network
+
+        :param str cmd: The POX URL to grab the topology
+        :return: The list of nodes and links detected
+        :rtype: dict
+        """
         return self._send_controller_res(cmd)
 
     def get_link_stats(self, cmd='get_link_stats'):
+        """Get links statistics over the pox controller
+
+        :param str cmd: The POX URL to grab the links statistics
+        :return: The controller response
+        :rtype: str
+        """
         return self._send_controller_res(cmd)
